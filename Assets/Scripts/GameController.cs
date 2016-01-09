921a01ed4450b8972a092c7887368f4174e7767f
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-09 18:14:35 +0900

    5-8

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public NejikoController nejiko;
+	public Text scoreLabel;
+	public LifePanel lifePanel;
+
+	public void Update () {
+		int score = CalcScore ();
+		scoreLabel.text = "Score : " + score + "m";
+		lifePanel.UpdateLife (nejiko.Life());
+	}
+
+	int CalcScore () 
+	{
+		return (int)nejiko.transform.position.z;
+	}
+}
