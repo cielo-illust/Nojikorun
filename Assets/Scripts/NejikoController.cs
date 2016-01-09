8dab8a447ae07e6abcc8c0717683e936eda37cb1
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-09 15:49:01 +0900

    5-4

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class NejikoController : MonoBehaviour {
+
+	const int MinLane = -2;
+	const int MaxLane = 2;
+	const float LaneWidth = 1.0f;
+
 	CharacterController controller;
 	Animator animator;
 
 	Vector3 moveDirection = Vector3.zero;
+	int targetLane;
 
 	public float gravity;
 	public float speedZ;
+	public float speedX;
 	public float speedJump;
+	public float accelerationZ;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +27,23 @@ public class NejikoController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown ("left")) {
+			MoveToLeft ();
+		}
+		if (Input.GetKeyDown ("right")) {
+			MoveToRight ();
+		}
+		if (Input.GetKeyDown ("space")) {
+			Jump ();
+		}
+
+		float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+		moveDirection.z = Mathf.Clamp (acceleratedZ, 0, speedZ);
+
+		float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+		moveDirection.x = ratioX * speedX;
+/*
 		if (controller.isGrounded) {
 			if (Input.GetAxis ("Vertical") > 0.0f) {
 				moveDirection.z = Input.GetAxis ("Vertical") * speedZ;
@@ -33,7 +58,7 @@ public class NejikoController : MonoBehaviour {
 				animator.SetTrigger ("jump");
 			}
 		}
-
+*/
 		moveDirection.y -= gravity * Time.deltaTime;
 
 		Vector3 globalDirection = transform.TransformDirection (moveDirection);
@@ -44,5 +69,25 @@ public class NejikoController : MonoBehaviour {
 		}
 
 		animator.SetBool ("run", moveDirection.z > 0.0f);
+	}
+
+	public void MoveToLeft()
+	{
+		if (controller.isGrounded && targetLane > MinLane) {
+			targetLane--;
+		}
+	}
+	public void MoveToRight()
+	{
+		if (controller.isGrounded && targetLane < MaxLane) {
+			targetLane++;
+		}
+	}
+	public void Jump()
+	{
+		if (controller.isGrounded) {
+			moveDirection.y = speedJump;
+			animator.SetTrigger ("jump");
+		}
 	}
 }
