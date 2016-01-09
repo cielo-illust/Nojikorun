64eacd963a8953d8ce38d4eacd37c57fd58e65dd
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2016-01-09 19:01:21 +0900

    alpha

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 	public NejikoController nejiko;
@@ -10,11 +11,25 @@ public class GameController : MonoBehaviour {
 	public void Update () {
 		int score = CalcScore ();
 		scoreLabel.text = "Score : " + score + "m";
+
 		lifePanel.UpdateLife (nejiko.Life());
+
+		if (nejiko.Life () <= 0) {
+			enabled = false;
+			if (PlayerPrefs.GetInt ("HighScore") < score) {
+				PlayerPrefs.SetInt ("HighScore", score);
+			}
+			Invoke ("ReturnToTitle", 2.0f);
+		}
 	}
 
 	int CalcScore () 
 	{
 		return (int)nejiko.transform.position.z;
+	}
+
+	void ReturnToTitle ()
+	{
+		SceneManager.LoadScene ("Title");
 	}
 }
---
+++ Assets/Scripts/TirleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TirleController : MonoBehaviour {
+	public Text highScoreLabel;
+
+	public void Start() {
+		highScoreLabel.text = "HighScore : " + PlayerPrefs.GetInt ("HighScore") + "m";
+	}
+	public void OnStartButtonChecked () {
+		SceneManager.LoadScene ("Main");
+	}
+}
